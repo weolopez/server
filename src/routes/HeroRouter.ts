8f6aa9040f0559200424266fa65707db249e8ae4
ml5174 <ml5174@att.com>
2017-05-05 17:51:05 -0400

    added nodegit

--- src/routes/HeroRouter.ts
+++ src/routes/HeroRouter.ts
@@ -1,4 +1,6 @@
-import {Router, Request, Response, NextFunction} from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
+var Git = require("nodegit");
+
 const Heroes = require('../data');
 
 export class HeroRouter {
@@ -19,28 +21,48 @@ export class HeroRouter {
     res.send(Heroes);
   }
 
-/**
- * GET one hero by id
- */
-public getOne(req: Request, res: Response, next: NextFunction) {
-  let query = parseInt(req.params.id);
-  let hero = Heroes.find(hero => hero.id === query);
-  if (hero) {
-    res.status(200)
-      .send({
-        message: 'Success',
-        status: res.status,
-        hero
-      });
+  /**
+   * GET one hero by id
+   */
+  public getOne(req: Request, res: Response, next: NextFunction) {
+    let query = parseInt(req.params.id);
+    let hero = Heroes.find(hero => hero.id === query);
+    if (hero) {
+      res.status(200)
+        .send({
+          message: 'Success',
+          status: res.status,
+          hero
+        });
+    }
+    else {
+      res.status(404)
+        .send({
+          message: 'No hero found with the given id.',
+          status: res.status
+        });
+    }
   }
-  else {
-    res.status(404)
+
+  public change(req: Request, res: Response, next: NextFunction) {
+
+    if (req.body.command === 'pull') {
+      Git.Clone("https://github.com/nodegit/nodegit", "nodegit").then(function(repository) {
+  // Work with the repository object here.
+      });
+      res.status(200)
+        .send({
+          message: req.body.command,
+          status: res.status
+        });
+    }
+    else res.status(200)
       .send({
-        message: 'No hero found with the given id.',
+        message: req.body.hello,
         status: res.status
       });
+
   }
-}
 
   /**
    * Take each handler, and attach to one of the Express.Router's
@@ -48,8 +70,8 @@ public getOne(req: Request, res: Response, next: NextFunction) {
    */
   init() {
     this.router.get('/', this.getAll);
-
-  this.router.get('/:id', this.getOne);
+    this.router.get('/:id', this.getOne);
+    this.router.post('/', this.change);
   }
 
 }
---
+++ typings/index.d.ts
@@ -0,0 +1,1 @@
+/// <reference path="modules/promisify-node/index.d.ts" />
